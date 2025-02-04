99d038b5d3536bc0e36f113f4ff7519bd273c06f
Memelero <124786659+Memelero@users.noreply.github.com>
2025-02-04 23:08:07 +0100

    mapak

--- E2_T5_WEB_V2/src/app/components/control-panel/school-list/details-school/details-school.component.ts
+++ E2_T5_WEB_V2/src/app/components/control-panel/school-list/details-school/details-school.component.ts
@@ -1,25 +1,59 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../../../services/bd.service';
 import { School } from '../../../../interface/school';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { CommonModule } from '@angular/common';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-details-school',
-  imports: [],
+  imports: [GoogleMapsModule,CommonModule],
   templateUrl: './details-school.component.html',
   styleUrl: './details-school.component.css'
 })
-export class DetailsSchoolComponent {
-  school: School | undefined;
+export class DetailsSchoolComponent implements OnInit{
+  ikastetxe: School | undefined;
+  map!: mapboxgl.Map;
+  marker!: mapboxgl.Marker;
 
   constructor(private route: ActivatedRoute, private apiService: ApiService) {}
 
   ngOnInit() {
-    // const schoolId = this.route.snapshot.paramMap.get('id'); // Obtén el ID de la ruta
-    // if (schoolId) {
-    //   this.apiService.getSchoolById(+schoolId).subscribe((school) => {
-    //     this.school = school;
-    //   });
-    // }
+    const id = this.route.snapshot.paramMap.get('id'); // Obtén el ID de la URL
+    if (id) {
+      this.loadSchoolDetails(+id);
+    }
+  }
+
+  loadSchoolDetails(id: number): void {
+    this.apiService.getSchools().subscribe({
+      next: (data) => {
+        this.ikastetxe = data.find((school) => school.CCEN === id);
+        if (this.ikastetxe) {
+          this.initializeMap();
+        }
+      },
+      error: (err) => {
+        console.error('Error al cargar los detalles del ikastetxe:', err);
+      }
+    });
+  }
+
+  initializeMap(): void {
+    // Configura el mapa
+    this.map = new mapboxgl.Map({
+      container: 'map', // ID del contenedor del mapa
+      accessToken: 'pk.eyJ1IjoiaXR6aS1hciIsImEiOiJjbTR0cnJvbmgwOG1xMmpyOXphYnk2YXA3In0.nvbObADvRjZvchA9t_gJog', // Tu token de Mapbox
+      style: 'mapbox://styles/mapbox/streets-v12', // Estilo del mapa
+      center: [this.ikastetxe!.LONGITUD, this.ikastetxe!.LATITUD], // Centro del mapa (longitud, latitud)
+      zoom: 15 // Nivel de zoom
+    });
+
+    // Agrega un marcador en la ubicación del ikastetxe
+    this.marker = new mapboxgl.Marker({ color: '#FF0000' }) // Color del marcador
+      .setLngLat([this.ikastetxe!.LONGITUD, this.ikastetxe!.LATITUD]) // Posición del marcador
+      .setPopup(new mapboxgl.Popup().setHTML(`<strong>${this.ikastetxe!.NOM}</strong><br>${this.ikastetxe!.DOMI}`)) // Popup con información
+      .addTo(this.map);
   }
 }
--- E2_T5_WEB_V2/src/app/components/control-panel/school-list/school-list.component.ts
+++ E2_T5_WEB_V2/src/app/components/control-panel/school-list/school-list.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../services/bd.service';
+import { School } from '../../../interface/school';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-school-list',
-  imports: [],
+  imports: [MatCardModule,CommonModule],
   templateUrl: './school-list.component.html',
   styleUrl: './school-list.component.css'
 })
-export class SchoolListComponent {
+export class SchoolListComponent implements OnInit{
+  ikastetxeak: School[] = []; // Usamos la interfaz School para tipar los datos
 
+  constructor(private ApiService: ApiService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadSchools();
+  }
+
+  loadSchools(): void {
+    this.ApiService.getSchools().subscribe({
+      next: (data) => {
+        this.ikastetxeak = data; // Asignamos los datos obtenidos a la propiedad ikastetxeak
+      },
+      error: (err) => {
+        console.error('Error al cargar los ikastetxes:', err);
+      }
+    });
+  }
+  viewSchoolDetails(school: School) {
+    this.router.navigate(['/school', school.CCEN]); // Redirige a la ruta anidada
+  }
 }
